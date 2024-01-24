e1a19bc4673eefd9dda1772c401af20f40134686
fatih akbulut <fatihakbulut997@gmail.com>
2024-01-25 00:28:58 +0300

    Son işlemeler yapıldı.

---
+++ Notlar/Sayfalar_Arasi_islemler/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sayfalar_Arasi_islemler"
+include(":app")
+ 
