9c32cb1d3a7aa2bbb6d9bfb829541a1578edee53
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-12 00:48:09 +0300

    Seekbar-ProgressBar kullanımı.

--- Notlar/ProgressBar-Slider-RatingBar/app/src/main/java/com/fatihakbulut/progressbar_slider_ratingbar/MainActivity.kt
+++ Notlar/ProgressBar-Slider-RatingBar/app/src/main/java/com/fatihakbulut/progressbar_slider_ratingbar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fatihakbulut.progressbar_slider_ratingbar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
 import com.fatihakbulut.progressbar_slider_ratingbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -23,5 +24,23 @@ class MainActivity : AppCompatActivity() {
 
             binding.progressBar.visibility = View.INVISIBLE // Görünmez
         }
+
+
+        binding.seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+
+            override fun onProgressChanged(seekBar: SeekBar?, ilerleme: Int, fromUser: Boolean) { // anlık sonuc verir (kaydırırken)
+
+                binding.textViewSonuc.text = "Sonuç : $ilerleme"
+
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {  // Seekbar'a dokunmaya başladığımızda çalışır.
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { //  Seeknar'ı durdurduğumuzda çalışır.
+
+            }
+        } )
     }
 }
