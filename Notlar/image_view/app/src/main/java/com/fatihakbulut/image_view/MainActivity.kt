ec0700ef10de97fa5bf404f9b72ced07d95a8f5b
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-12 10:37:09 +0300

    Resimler drawable dosyasına eklendi.

---
+++ Notlar/image_view/app/src/main/java/com/fatihakbulut/image_view/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.fatihakbulut.image_view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fatihakbulut.image_view.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
