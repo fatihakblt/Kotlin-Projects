791c79523598df7200e9bda6999acfacd8395e78
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-12 11:18:25 +0300

    Video View kullanımı.

--- Notlar/Video-View/app/src/main/java/com/fatihakbulut/video_view/MainActivity.kt
+++ Notlar/Video-View/app/src/main/java/com/fatihakbulut/video_view/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.fatihakbulut.video_view
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fatihakbulut.video_view.databinding.ActivityMainBinding
@@ -12,6 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        
+
+        binding.buttonBasla.setOnClickListener {
+
+            val adres = Uri.parse("android.resource://"+packageName+"/"+R.raw.video)
+            binding.videoView.setVideoURI(adres)
+            binding.videoView.start()
+        }
+
+        binding.buttonDur.setOnClickListener {
+
+            binding.videoView.stopPlayback()
+
+
+        }
+
     }
 }
