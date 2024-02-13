84eeb9cace098505151f63172be1572bd2af092c
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-13 23:13:01 +0300

    Menu'deki item'lara bağlantı sağlandı.

--- Notlar/Pop-Up-Menu/app/src/main/java/com/fatihakbulut/popupmenu/MainActivity.kt
+++ Notlar/Pop-Up-Menu/app/src/main/java/com/fatihakbulut/popupmenu/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fatihakbulut.popupmenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.PopupMenu
 import com.fatihakbulut.popupmenu.databinding.ActivityMainBinding
 
@@ -15,6 +16,25 @@ class MainActivity : AppCompatActivity() {
         binding.buttonMenuAc.setOnClickListener {
             val popup = PopupMenu(this@MainActivity,binding.buttonMenuAc) // İkinci değişken hangi görsel nesnenin popup menusunu açacağını ifade eder.
             popup.menuInflater.inflate(R.menu.popup_menu,popup.menu)
+
+            // Menu'den yapılan seçimi yakalama
+            popup.setOnMenuItemClickListener {item ->
+                when(item.itemId){
+                    R.id.action_sil -> {
+                        Toast.makeText(applicationContext,"Sil seçildi",Toast.LENGTH_SHORT).show()
+                        true// Bu ifade click listener'a gönderiliyor ve bu item'a tıklanıldığı anlaşılıyor.
+
+                    }
+
+                    R.id.action_duzenle ->{
+                        Toast.makeText(applicationContext,"Düzenle seçildi",Toast.LENGTH_SHORT).show()
+                        true
+
+                    }
+                    else -> false // Bu ifade click listener'a gönderiliyor ve bu item'a tıklanılmadığı!!! anlaşılıyor.
+
+                }
+            }
 
             popup.show()
         }
