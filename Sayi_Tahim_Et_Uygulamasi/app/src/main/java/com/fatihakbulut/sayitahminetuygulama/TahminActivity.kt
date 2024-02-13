a37fdd2797e7ab265ea73ddf116d3d01512885fc
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-13 16:11:29 +0300

    Tahmin sayfasındaki input tipi ayarlandı ve bu sayfa oluşturulduğunda 0-100 arası rastgele sayı üretildi.

--- Sayi_Tahim_Et_Uygulamasi/app/src/main/java/com/fatihakbulut/sayitahminetuygulama/TahminActivity.kt
+++ Sayi_Tahim_Et_Uygulamasi/app/src/main/java/com/fatihakbulut/sayitahminetuygulama/TahminActivity.kt
@@ -3,21 +3,35 @@ package com.fatihakbulut.sayitahminetuygulama
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.fatihakbulut.sayitahminetuygulama.databinding.ActivityTahminBinding
+import kotlin.random.Random
 
 class TahminActivity : AppCompatActivity() {
+    // Rastgele sayı üretme ve sayaç global değişkenleri
+    private var rastgeleSayi = 0
+    private var sayac = 5
+
+
     private lateinit var binding: ActivityTahminBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTahminBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        rastgeleSayi = Random.nextInt(101) // 0 ile 100 arası rastgele sayı üretimi
+
+        Log.e("Rastgele Sayı", rastgeleSayi.toString()) // Tahmin edilen sayının testi için konsola bilgi verilmesi
+
 
         binding.buttonTahmin.setOnClickListener {
 
-            val intent = Intent(this@TahminActivity,SonucActivity:: class.java)
-            finish()
-            startActivity(intent)
+
+            //val intent = Intent(this@TahminActivity,SonucActivity:: class.java)
+            //finish()
+            //startActivity(intent)
+
+            
         }
     }
 }
