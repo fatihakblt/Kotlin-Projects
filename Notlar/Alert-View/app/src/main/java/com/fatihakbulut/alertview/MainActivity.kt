cd4c07e1f841344986c48cb711e8b221318a8ff5
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-13 23:38:24 +0300

    Alert View oluşturuldu.

--- Notlar/Alert-View/app/src/main/java/com/fatihakbulut/alertview/MainActivity.kt
+++ Notlar/Alert-View/app/src/main/java/com/fatihakbulut/alertview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.fatihakbulut.alertview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.fatihakbulut.alertview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // Basit AlertView  oluşturma
+        binding.buttonNormal.setOnClickListener {
+            val ad = AlertDialog.Builder(this@MainActivity)
+
+            ad.setMessage("Mesaj")
+            ad.setTitle("Başlık")
+            ad.setIcon(R.drawable.resim)
+
+            ad.setPositiveButton("Tamam"){dialogInterface, i->
+
+                Toast.makeText(applicationContext,"Tamam Tıklanıldı", Toast.LENGTH_SHORT).show()
+            }
+
+            ad.setNegativeButton("İptal"){dialogInterface, i->
+
+                Toast.makeText(applicationContext,"İptal Tıklanıldı", Toast.LENGTH_SHORT).show()
+
+            }
+
+            ad.create().show()
 
 
-        
+        }
+
+        binding.buttonOzel.setOnClickListener {
+
+
+        }
+
     }
 }
