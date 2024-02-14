46c42339d2e3dc4250157fb6484fc06add825145
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-14 13:53:24 +0300

    Özelleştirilmiş Alert view yapısı.

--- Notlar/Alert-View/app/src/main/java/com/fatihakbulut/alertview/MainActivity.kt
+++ Notlar/Alert-View/app/src/main/java/com/fatihakbulut/alertview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fatihakbulut.alertview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.fatihakbulut.alertview.databinding.ActivityMainBinding
@@ -37,6 +38,36 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonOzel.setOnClickListener {
+
+            val tasarim = layoutInflater.inflate(R.layout.alert_tasarim,null) // Tasarım ile bağlantı kurduk
+
+            val editTextAlert = tasarim.findViewById(R.id.editTextAlert) as EditText // Tasarım içindeki görsel nesneye ulaştık.
+
+            val ad = AlertDialog.Builder(this@MainActivity)
+
+            ad.setMessage("Mesaj")
+            ad.setTitle("Başlık")
+            ad.setIcon(R.drawable.resim)
+
+            ad.setView(tasarim) // Tasarımı :Alert View'a ekledik
+
+            ad.setPositiveButton("Kaydet"){dialogInterface, i->
+
+                val alinanVeri = editTextAlert.text.toString()
+
+                Toast.makeText(applicationContext,"Alınan Veri : $alinanVeri", Toast.LENGTH_SHORT).show()
+            }
+
+            ad.setNegativeButton("İptal"){dialogInterface, i->
+
+                Toast.makeText(applicationContext,"İptal Tıklanıldı", Toast.LENGTH_SHORT).show()
+
+            }
+
+            ad.create().show()
+
+
+
 
 
         }
