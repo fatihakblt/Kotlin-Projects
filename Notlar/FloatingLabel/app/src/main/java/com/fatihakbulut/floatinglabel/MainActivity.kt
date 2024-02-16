de0b8ffdec8620439f5e42ae971952d9057f3721
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-16 09:32:33 +0300

    editText'in boş geçilmesi durumu kontrol edildi ve tel bilgisine özel şart eklendi.

--- Notlar/FloatingLabel/app/src/main/java/com/fatihakbulut/floatinglabel/MainActivity.kt
+++ Notlar/FloatingLabel/app/src/main/java/com/fatihakbulut/floatinglabel/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fatihakbulut.floatinglabel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import com.fatihakbulut.floatinglabel.databinding.ActivityMainBinding
 
@@ -16,6 +17,24 @@ class MainActivity : AppCompatActivity() {
             // Alınan metinlerdeki boşlukları silmek için trim metodu kullanıldı.(whitespace boşlukların silinmesi)
             val ad = binding.editTextAd.text.toString().trim()
             val tel = binding.editTextTel.text.toString().trim()
+
+            // ediTextlerin içinin boş olması durumunun kontrolü yapıldı
+            // editTextler dolu ise devam eder dolu değilse buttonun çalışmasını durdurur.
+            if(TextUtils.isEmpty(ad)){
+                Toast.makeText(applicationContext,"Ad Boş", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+            if (TextUtils.isEmpty(tel)){
+                Toast.makeText(applicationContext,"Tel Boş", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+            // Tel uzunluğunun minimum 6 haneli olması şartı getirildi.
+            if (tel.length<6){
+                Toast.makeText(applicationContext,"Tel en az 6 hane olmalıdır.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
 
             Toast.makeText(this@MainActivity,"$ad - $tel",Toast.LENGTH_SHORT).show()
