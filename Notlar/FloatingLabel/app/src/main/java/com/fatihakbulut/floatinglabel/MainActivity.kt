9497ceea5d122604b933bcd73424731f7181a9c6
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-16 09:05:35 +0300

    editText'ten veriler alındı, Toast mesaj olarak uygulamada bildirim verildi.

--- Notlar/FloatingLabel/app/src/main/java/com/fatihakbulut/floatinglabel/MainActivity.kt
+++ Notlar/FloatingLabel/app/src/main/java/com/fatihakbulut/floatinglabel/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fatihakbulut.floatinglabel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.fatihakbulut.floatinglabel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonYap.setOnClickListener {
+            val ad = binding.editTextAd.text.toString()
+            val tel = binding.editTextTel.text.toString()
+
+
+            Toast.makeText(this@MainActivity,"$ad - $tel",Toast.LENGTH_SHORT).show()
+        }
     }
 }
