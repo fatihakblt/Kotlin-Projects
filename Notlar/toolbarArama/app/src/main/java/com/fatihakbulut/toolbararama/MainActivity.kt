3c8ebde330c0851f7073bb6cb463046523261a30
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-18 18:13:24 +0300

    Menu tasarımı oluşturuldu ve toolbar'a eklendi.

--- Notlar/toolbarArama/app/src/main/java/com/fatihakbulut/toolbararama/MainActivity.kt
+++ Notlar/toolbarArama/app/src/main/java/com/fatihakbulut/toolbararama/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fatihakbulut.toolbararama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.fatihakbulut.toolbararama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        binding.toolbar.title ="Arama"
+        setSupportActionBar(binding.toolbar)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_arama, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
