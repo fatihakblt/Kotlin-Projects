47ea7ecfbbc2aeb41ee7ef2f3310d7a40a211805
fatih akbulut <fatihakbulut997@gmail.com>
2024-02-22 14:12:01 +0300

    Shared preferences işlemi uygulanarak sayac değeri lokal hafızaya eklendi.

--- giris-sayac-uygulamasi/app/src/main/java/com/fatihakbulut/giris_sayac_uygulamasi/MainActivity.kt
+++ giris-sayac-uygulamasi/app/src/main/java/com/fatihakbulut/giris_sayac_uygulamasi/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.fatihakbulut.giris_sayac_uygulamasi
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fatihakbulut.giris_sayac_uygulamasi.databinding.ActivityMainBinding
@@ -11,6 +12,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        val sp = getSharedPreferences("GirisSayici", Context.MODE_PRIVATE) // Bilgilerin kaydedileceği dosya oluşturuldu.
+
+        var sayac = sp.getInt("sayac",0) // (key,varsayılan değer), sayac değerini okuduk.Değer olmadığı için 0 değerini atadı.
+
+        val editor = sp.edit() // Dosya üzerinde silme güncelleme (kayıt işlemi) işlemi yapmak için editor değişkenini oluşturduk.
+        editor.putInt("sayac",++sayac) // Sayac değişkenini bir arttırdık.
+
+        editor.commit() // işlemler tamamlandı
+        binding.textViewSayac.text = "Açılış Sayısı : $sayac"
+
+
     }
 }
