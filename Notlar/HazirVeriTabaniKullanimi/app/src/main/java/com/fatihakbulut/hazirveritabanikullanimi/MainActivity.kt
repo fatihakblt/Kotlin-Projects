ddd8cafa84e81649a04ca9aa3924d5e62920c5b6
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-16 12:25:22 +0300

    Veritabanı lokal belleğe kopyalandı.

--- Notlar/HazirVeriTabaniKullanimi/app/src/main/java/com/fatihakbulut/hazirveritabanikullanimi/MainActivity.kt
+++ Notlar/HazirVeriTabaniKullanimi/app/src/main/java/com/fatihakbulut/hazirveritabanikullanimi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fatihakbulut.hazirveritabanikullanimi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fatihakbulut.hazirveritabanikullanimi.databinding.ActivityMainBinding
+import com.info.sqlitekullanimihazirveritabani.DatabaseCopyHelper
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -11,6 +12,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        veritabaniKopyala()
 
+    }
+
+    // Veritabanını kopyalama işlemini yapan fonksiyon
+    fun veritabaniKopyala(){
+
+        val db = DatabaseCopyHelper(this)
+
+        try {
+            db.createDataBase() // Bu uygulama hangi telefonda çalışıyorsa o telefonda filmler.sqlite veritabanını kopyalar.
+        }catch (e:Exception){
+            e.printStackTrace() // Hata sonucunu açıklamalı olarak verir.
+        }
+
+        try {
+            db.openDataBase()// Bu uygulama hangi telefonda çalışıyorsa o telefonda filmler.sqlite veritabanını aktif hale getirir.
+        }catch (e:Exception){
+            e.printStackTrace() // Hata sonucunu açıklamalı olarak verir.
+        }
     }
 }
