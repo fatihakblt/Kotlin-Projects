97845860f85dd9312ef15d7c9bdc329664d8c3d6
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-18 16:28:58 +0300

    Adapter sınıfı oluşturuldu Toolbar ve recyclerview ile ilgili ön kodlamar yapıldı.

--- Sozluk_Uygulamasi/app/src/main/java/com/fatihakbulut/sozlukuygulamasi/Kelimeler.kt
+++ Sozluk_Uygulamasi/app/src/main/java/com/fatihakbulut/sozlukuygulamasi/Kelimeler.kt
@@ -4,6 +4,6 @@ import java.io.Serializable
 
 data class Kelimeler (var kelime_id:Int, var ingilizce:String, var turkce:String ):Serializable{
 
-    
+
 
 }
--- Sozluk_Uygulamasi/app/src/main/java/com/fatihakbulut/sozlukuygulamasi/MainActivity.kt
+++ Sozluk_Uygulamasi/app/src/main/java/com/fatihakbulut/sozlukuygulamasi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fatihakbulut.sozlukuygulamasi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fatihakbulut.sozlukuygulamasi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.toolbar.title = "Sözlük Uygulaması"
+        setSupportActionBar(binding.toolbar)
+
+        binding.rv.setHasFixedSize(true)
+        binding.rv.layoutManager = LinearLayoutManager(this)
     }
 }
