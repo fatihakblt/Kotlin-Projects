ebcc151e4e84ee8ad62bd957f197fd5c6dcefe51
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-18 16:06:11 +0300

    Veritabanı sınıf modeli oluşturuldu.

---
+++ Sozluk_Uygulamasi/app/src/main/java/com/fatihakbulut/sozlukuygulamasi/Kelimeler.kt
@@ -0,0 +1,9 @@
+package com.fatihakbulut.sozlukuygulamasi
+
+import java.io.Serializable
+
+data class Kelimeler (var kelime_id:Int, var ingilizce:String, var turkce:String ):Serializable{
+
+    
+
+}
