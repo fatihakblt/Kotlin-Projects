38d3406c0e5e2b3e06ef5341a5364b462080f844
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-27 15:49:01 +0300

    BroadcastReceiver kullanılarak ANDROİD LİSTESİNDE OLMAYAN ÖZELLİK İÇİN FİLTRELEME YAPILDI ve Toast mesaj oluşturuldu.

--- Broadcast_receiver_sarj_seviyesi_kontrol_uygulamasi/app/src/main/java/com/fatihakbulut/sarjseviyekontroluygulamasi/MainActivity.kt
+++ Broadcast_receiver_sarj_seviyesi_kontrol_uygulamasi/app/src/main/java/com/fatihakbulut/sarjseviyekontroluygulamasi/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.fatihakbulut.sarjseviyekontroluygulamasi
 
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fatihakbulut.sarjseviyekontroluygulamasi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var sarjSeviyeAlgilama: SarjSeviyeAlgilama // SarjSeviyeAlgilama sınıfından boş bir nesne oluşturduk.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        sarjSeviyeAlgilama = SarjSeviyeAlgilama() // nesne tanımlandı.
+
+
+
+    }
+
+    // Uygulama çalıştığında BATTERY_LOW filtresini yakalar - düşük batarya filtresi(%15 ve altı)**
+    override fun onResume() {
+        super.onResume()
+        val filter = IntentFilter()
+        filter.addAction("android.intent.action.BATTERY_LOW")
+        // BroadCast aktif edildi
+        registerReceiver(sarjSeviyeAlgilama,filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // BroadCast deaktif edildi
+        unregisterReceiver(sarjSeviyeAlgilama)
     }
 }
--- Broadcast_receiver_sarj_seviyesi_kontrol_uygulamasi/app/src/main/java/com/fatihakbulut/sarjseviyekontroluygulamasi/SarjSeviyeAlgilama.kt
+++ Broadcast_receiver_sarj_seviyesi_kontrol_uygulamasi/app/src/main/java/com/fatihakbulut/sarjseviyekontroluygulamasi/SarjSeviyeAlgilama.kt
@@ -7,7 +7,7 @@ import android.widget.Toast
 
 class SarjSeviyeAlgilama : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Toast.makeText(context,"Şarjınız bitmek üzere :(",Toast.LENGTH_SHORT).show()
+        Toast.makeText(context,"Şarjınız bitmek üzere :(",Toast.LENGTH_SHORT).show() // Broadcast aldığında bu kısım çalışır.
     }
 
 }
