c7ba2204aacb4a2298ce13ae5e5690a064708741
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-27 15:31:21 +0300

    BroadcastReceiver için sınıf oluşturuldu.

---
+++ Broadcast_receiver_sarj_seviyesi_kontrol_uygulamasi/app/src/main/java/com/fatihakbulut/sarjseviyekontroluygulamasi/SarjSeviyeAlgilama.kt
@@ -0,0 +1,13 @@
+package com.fatihakbulut.sarjseviyekontroluygulamasi
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class SarjSeviyeAlgilama : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context,"Şarjınız bitmek üzere :(",Toast.LENGTH_SHORT).show()
+    }
+
+}
