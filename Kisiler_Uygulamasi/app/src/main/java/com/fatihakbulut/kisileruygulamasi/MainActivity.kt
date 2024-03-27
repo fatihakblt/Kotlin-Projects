e264c9deb305ed5b8390fd042a8a66728591ebf4
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-27 11:36:33 +0300

    Kisi ekleme ve kisileri listeleme fonksiyonu eklendi ve uygulandı.

--- Kisiler_Uygulamasi/app/src/main/java/com/fatihakbulut/kisileruygulamasi/MainActivity.kt
+++ Kisiler_Uygulamasi/app/src/main/java/com/fatihakbulut/kisileruygulamasi/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity(),androidx.appcompat.widget.SearchView.On
     private lateinit var binding: ActivityMainBinding
     private lateinit var kisilerListe: ArrayList<Kisiler>
     private lateinit var adapter: KisilerAdapter
+    private lateinit var vt : VeritabaniYardimcisi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -30,19 +31,20 @@ class MainActivity : AppCompatActivity(),androidx.appcompat.widget.SearchView.On
         binding.rv.setHasFixedSize(true) // RV'ın daha iyi görünmesini sağlar.
         binding.rv.layoutManager = LinearLayoutManager(this) // RecyclerView yapısının alta alta görünmesini sağlayan kodlama
 
-        // Veri kümesinin oluşturulması
+        /*// Veri kümesinin oluşturulması
         kisilerListe = ArrayList()
         val k1 = Kisiler(1,"Ahmet","888888")
         val k2 = Kisiler(2,"Zeynep","666666")
 
         kisilerListe.add(k1)
-        kisilerListe.add(k2)
+        kisilerListe.add(k2)*/
 
-        // Veri kümesinin adapter sınıfına aktarılması
-        adapter = KisilerAdapter(this,kisilerListe)
+        // Veritabanı işlemleri
+        vt = VeritabaniYardimcisi(this)
 
-        // Adapter nesnesinin RecyclerView'a aktarılması
-        binding.rv.adapter = adapter
+        // Veritabanı üzerindeki işlemler
+        tumKisilerAl()
+
 
         // Floating Action button işlemleri
         binding.fab.setOnClickListener {
@@ -74,6 +76,10 @@ class MainActivity : AppCompatActivity(),androidx.appcompat.widget.SearchView.On
             val kisi_ad = editTextAd.text.toString().trim()
             val kisi_tel = editTextTel.text.toString().trim()
 
+            // Veritabanı üzerinde kayıt işleminin yapılması
+            Kisilerdao().kisiEkle(vt,kisi_ad,kisi_tel)
+            // Güncellenen veriler ana sayfaya tekrar çekilmeli
+            tumKisilerAl()
             Toast.makeText(applicationContext,"$kisi_ad - $kisi_tel",Toast.LENGTH_SHORT).show()
 
         }
@@ -95,6 +101,18 @@ class MainActivity : AppCompatActivity(),androidx.appcompat.widget.SearchView.On
             Log.e("Harf girdikçe",newText)
         }
         return true
+    }
+
+    fun tumKisilerAl(){
+        // Tüm kişileri listeleme
+        kisilerListe = Kisilerdao().tumKisiler(vt)
+
+        // Veri kümesinin adapter sınıfına aktarılması
+        adapter = KisilerAdapter(this,kisilerListe)
+
+        // Adapter nesnesinin RecyclerView'a aktarılması
+        binding.rv.adapter = adapter
+
     }
 
 }
