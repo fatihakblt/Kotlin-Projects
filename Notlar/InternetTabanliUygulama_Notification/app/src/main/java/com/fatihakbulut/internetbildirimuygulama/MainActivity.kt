10452ee2f1f4951eae9bc3a68606e2f7480f3ac7
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-28 15:24:02 +0300

    Firebase cloud messaging bağlantısı kuruldu.

--- Notlar/InternetTabanliUygulama_Notification/app/src/main/java/com/fatihakbulut/internetbildirimuygulama/MainActivity.kt
+++ Notlar/InternetTabanliUygulama_Notification/app/src/main/java/com/fatihakbulut/internetbildirimuygulama/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
+
     }
 }
