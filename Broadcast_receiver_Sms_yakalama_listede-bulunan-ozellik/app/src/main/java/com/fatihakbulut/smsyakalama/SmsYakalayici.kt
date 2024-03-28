57f7a747d565fef697308df64c26fbf4c417b0d4
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-28 11:58:53 +0300

    Sms gelen tel no ve sms detayına ulaşıldı.

--- Broadcast_receiver_Sms_yakalama_listede-bulunan-ozellik/app/src/main/java/com/fatihakbulut/smsyakalama/SmsYakalayici.kt
+++ Broadcast_receiver_Sms_yakalama_listede-bulunan-ozellik/app/src/main/java/com/fatihakbulut/smsyakalama/SmsYakalayici.kt
@@ -3,12 +3,39 @@ package com.fatihakbulut.smsyakalama
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.telephony.SmsMessage
+import android.telephony.SmsMessage.createFromPdu
 import android.widget.Toast
 
 class SmsYakalayici :BroadcastReceiver(){
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        Toast.makeText(context,"SMS GELDİ",Toast.LENGTH_SHORT).show()
+        val b = intent?.extras
+
+        if (b != null){
+            val pdusObj = b.get("pdus") as Array<Any>
+
+            for (i in pdusObj.indices){ // 0..2 ->0,1,2
+
+                val guncelMesaj:SmsMessage
+
+                // Mesaja ulaşma
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){ // Güncel kodlama
+                    val format = b.getString("format")
+                    guncelMesaj = createFromPdu(pdusObj[i] as ByteArray,format)
+                    // Eski versiyon kodlama
+                }else{
+                    guncelMesaj = createFromPdu(pdusObj[i] as ByteArray)
+                }
+                val telNo = guncelMesaj.displayOriginatingAddress
+                val mesaj = guncelMesaj.displayMessageBody
+
+                Toast.makeText(context,"$telNo - $mesaj",Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
 
     }
 }
