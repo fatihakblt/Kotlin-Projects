444c9e3bb88e8675cd36d14684ffeaa9200bb2c7
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-28 12:10:11 +0300

    View Binding işlemi yapıldı.

---
+++ Notlar/DurumaBagliBildirim_Notification/app/src/main/java/com/fatihakbulut/durumabaglibildirim/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.fatihakbulut.durumabaglibildirim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fatihakbulut.durumabaglibildirim.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
