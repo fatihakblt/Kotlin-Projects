5f56c6f02202cac9c31ece70b91bdd92bccc425e
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-29 15:14:15 +0300

    Ekranda geri sayım oluşturuldu

--- Notlar/Count_Down_timer_notification/app/src/main/java/com/fatihakbulut/countdowntimer/MainActivity.kt
+++ Notlar/Count_Down_timer_notification/app/src/main/java/com/fatihakbulut/countdowntimer/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fatihakbulut.countdowntimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.fatihakbulut.countdowntimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.buttonBasla.setOnClickListener {
+            val sayici = object : CountDownTimer(30000,1000) { // 30 saniyeden 1'er saniye aralıklarla çalısacak zamanlayıcı
+
+                override fun onTick(i: Long) { // Her azalmada çalışan metod
+
+                    binding.textViewCikti.text = "Kalan Süre : ${i/1000} sn"
+                }
+
+                override fun onFinish() { // İşlem bittiğinde çalışan metod
+
+                    binding.textViewCikti.text = "Bitti"
+                }
+            }
+
+            sayici.start() // Sayıcıyı başlattık.
+        }
     }
 }
