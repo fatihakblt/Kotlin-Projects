060d752c8e0f1c50cb294895fc2b4edf059e4a02
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-29 17:02:03 +0300

    WorkManger için çalışma koşulu oluşturuldu.

--- Notlar/WorkManagerKullanimi/app/src/main/java/com/fatihakbulut/workmanagerkullanimi/MainActivity.kt
+++ Notlar/WorkManagerKullanimi/app/src/main/java/com/fatihakbulut/workmanagerkullanimi/MainActivity.kt
@@ -3,6 +3,8 @@ package com.fatihakbulut.workmanagerkullanimi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.fatihakbulut.workmanagerkullanimi.databinding.ActivityMainBinding
@@ -16,10 +18,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonYap.setOnClickListener {
+            val calismaKosulu = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED) // Internet varsa koşulu getirildi.
+                .build()
 
             // Butona bastıkça bir seferlik sonuç getiren yapı
             val istek = OneTimeWorkRequestBuilder<MyWorker>()
                 .setInitialDelay(10,TimeUnit.SECONDS) // 10 saniyelik gecikme eklendi
+                .setConstraints(calismaKosulu)
                 .build()
 
             WorkManager.getInstance(this).enqueue(istek)
