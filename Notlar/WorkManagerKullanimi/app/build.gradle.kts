1280ea0f3b0946b0b3efc70a793f47538863d62b
fatih akbulut <fatihakbulut997@gmail.com>
2024-03-29 16:24:02 +0300

    Work Manager kurulumu yapıldı.

--- Notlar/WorkManagerKullanimi/app/build.gradle.kts
+++ Notlar/WorkManagerKullanimi/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    val work_version = "2.9.0"
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -48,4 +48,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.work:work-runtime-ktx:$work_version")
+
 }
--- Notlar/WorkManagerKullanimi/app/src/main/java/com/fatihakbulut/workmanagerkullanimi/MainActivity.kt
+++ Notlar/WorkManagerKullanimi/app/src/main/java/com/fatihakbulut/workmanagerkullanimi/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        binding.buttonYap.setOnClickListener {
+            
+        }
     }
 }
