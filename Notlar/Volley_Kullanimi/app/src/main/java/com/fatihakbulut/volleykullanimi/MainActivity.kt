7b2c7c62a1dc9368ec6970c09ca729196f2e4118
fatih akbulut <fatihakbulut997@gmail.com>
2024-04-07 01:20:45 +0300

    delete_kisiler web servisi volley kütüphanesi ile çalıştırıldı.

--- Notlar/Volley_Kullanimi/app/src/main/java/com/fatihakbulut/volleykullanimi/MainActivity.kt
+++ Notlar/Volley_Kullanimi/app/src/main/java/com/fatihakbulut/volleykullanimi/MainActivity.kt
@@ -2,6 +2,10 @@ package com.fatihakbulut.volleykullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.fatihakbulut.volleykullanimi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        kisiSil()
+    }
+
+    fun kisiSil(){
+        // Web servisi ile iletişimin kurulması
+        val url = "https://fatihakbulut.net/kisiler/delete_kisiler.php"
+
+        val istek = object : StringRequest(Method.POST,url,Response.Listener { cevap ->
+            Log.e("Silme İşlemi Cevap",cevap)
+        },Response.ErrorListener { error ->error.printStackTrace()  }){
+
+            override fun getParams() : MutableMap<String, String>? {
+
+                val params = HashMap<String,String>()
+
+                params["kisi_id"] = "1"
+
+                return params
+            }
+        }
+
+        // isteğin çalıştırılması
+        Volley.newRequestQueue(this@MainActivity).add(istek)
     }
 }
