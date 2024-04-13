c0331a5c4e09d35909324a2777f76992376f1b2d
fatih akbulut <fatihakbulut997@gmail.com>
2024-04-13 23:52:49 +0300

    Retrofit ile Delete  işlemi yapıldı.

--- Notlar/RetrofitKullanimi/app/src/main/java/com/fatihakbulut/retrofitkullanimi/KisilerDaoInterface.kt
+++ Notlar/RetrofitKullanimi/app/src/main/java/com/fatihakbulut/retrofitkullanimi/KisilerDaoInterface.kt
@@ -1,4 +1,15 @@
 package com.fatihakbulut.retrofitkullanimi
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 // Bu interface içerisinde web servisler ile ilgili metotlar oluşturulacak ve daha sonra bunlar kullanılacak
 interface KisilerDaoInterface {
+
+    @POST("kisiler/delete_kisiler.php") // Kullanıcak web servisi
+    @FormUrlEncoded // Türkçe karakter desteği sağlıyor
+
+    fun kisiSil(@Field("kisi_id") kisi_id:Int): retrofit2.Call<CRUDCevap> // kisi_id Field'ına kisi_id değeri verilecek ve web servis çalıştırıldıktan sonra CRUDCevap sonucu döndürülecek
+
 }
--- Notlar/RetrofitKullanimi/app/src/main/java/com/fatihakbulut/retrofitkullanimi/MainActivity.kt
+++ Notlar/RetrofitKullanimi/app/src/main/java/com/fatihakbulut/retrofitkullanimi/MainActivity.kt
@@ -2,7 +2,11 @@ package com.fatihakbulut.retrofitkullanimi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.fatihakbulut.retrofitkullanimi.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -10,5 +14,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Kisi silme işlemi
+        kisiSil()
+
+    }
+    // Silme işlemi
+    fun kisiSil(){
+        // Silme işlemi
+        val kdi = ApiUtils.getKisilerDaoInterface()
+        kdi.kisiSil(3).enqueue(object : Callback<CRUDCevap>{
+
+            // Gelen cevap
+            override fun onResponse(call: Call<CRUDCevap>?, response: Response<CRUDCevap>?) {
+
+                if (response != null){
+                    Log.e("Başarı",response.body().success.toString())
+                    Log.e("Mesaj",response.body().message)
+
+                }
+
+
+            }
+
+            // Hata alınırsa hata sonucunu döndüren metod
+            override fun onFailure(call: Call<CRUDCevap>?, t: Throwable?) {
+
+
+            }
+        })
     }
 }
