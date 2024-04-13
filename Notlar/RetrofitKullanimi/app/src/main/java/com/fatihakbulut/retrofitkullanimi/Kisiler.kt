9b16746f6af1724a3a78873487f69836a25922b1
fatih akbulut <fatihakbulut997@gmail.com>
2024-04-13 22:31:06 +0300

    Json cevapları için sınıflar oluşturuldu.

---
+++ Notlar/RetrofitKullanimi/app/src/main/java/com/fatihakbulut/retrofitkullanimi/Kisiler.kt
@@ -0,0 +1,16 @@
+package com.fatihakbulut.retrofitkullanimi
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Kisiler(
+    @SerializedName("kisi_id")
+    @Expose
+    var kisiId:Int,
+    @SerializedName("kisi_ad")
+    @Expose
+    var kisiAd:String,
+    @SerializedName("kisi_tel")
+    @Expose
+    var  kisiTel:String) {
+}
