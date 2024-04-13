303e39048351d76a99df6d769be6331cf15769b3
fatih akbulut <fatihakbulut997@gmail.com>
2024-04-13 22:11:11 +0300

    Retrofit kütüphanesi ve Json dönüştürme kütüphaneleri eklendi.

--- Notlar/RetrofitKullanimi/app/build.gradle.kts
+++ Notlar/RetrofitKullanimi/app/build.gradle.kts
@@ -48,4 +48,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.1.0")
+
+
+    // Json Parsin
+    implementation("com.google.code.gson:gson:2.10")
+    implementation("com.squareup.retrofit2:converter-gson:2.1.0")
+
+
+
 }
